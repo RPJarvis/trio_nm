d5e9920c3b51f23fc19922eb4ff1b99626a4f1f2
JARVIS <RJarvis1@cnm.edu>
2016-08-19 15:03:17 -0600

    news and officer models

--- trio_nm/trio_nm_main/admin.py
+++ trio_nm/trio_nm_main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class NewsItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'body', 'optional_image', 'publish_date']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(models.NewsItem, NewsItemAdmin)
+
+
+class OfficerListingAdmin(admin.ModelAdmin):
+    list_display = ['trio_title', 'name', 'job_title', 'job_details', 'phone', 'optional_phone',
+                    'optional_fax', 'email']
+
+
+admin.site.register(models.OfficerListing, OfficerListingAdmin)
--- trio_nm/trio_nm_main/models.py
+++ trio_nm/trio_nm_main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class NewsItem(models.Model):
+    title = models.CharField(max_length=140)
+    slug = models.SlugField(unique=True, max_length=140)
+    body = models.TextField()
+    optional_image = models.ImageField(verbose_name="Optional Image", blank=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-publish_date']
+
+    def __unicode__(self):
+        return u'%s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('newsitem.views.post', args=[self.slug])
+
+
+class OfficerListing(models.Model):
+    trio_title = models.CharField(max_length=40)
+    name = models.CharField(max_length=60)
+    job_title = models.CharField(max_length=80)
+    job_details = models.CharField(max_length=160)
+    phone = models.CharField(max_length=14)
+    optional_phone = models.CharField(max_length=14, blank=True)
+    optional_fax = models.CharField(max_length=14, blank=True)
+    email = models.EmailField()
